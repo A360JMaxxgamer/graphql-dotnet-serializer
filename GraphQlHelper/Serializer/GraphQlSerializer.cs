c3e8e7a852544fba6b22b5e7d8273e5c36cc3ec6
Marco Hamann <marcohamann89@hotmail.de>
2019-01-07 20:11:07 +0100

    added an optional parameter whether all propertynames shall be in lowercase

--- GraphQlHelper/Serializer/GraphQlSerializer.cs
+++ GraphQlHelper/Serializer/GraphQlSerializer.cs
@@ -13,17 +13,20 @@ namespace GraphQlHelper.Serializer
         /// Serializes the <paramref name="obj"/> to a string representation which can be used as an input for graphql-dotnet.
         /// </summary>
         /// <param name="obj"></param>
+        /// <param name="propertyNameToLowerCase">An optional parameter to create the property names in lower case</param>
         /// <returns>A serialized version of the <paramref name="obj"/>.</returns>
-        public string Serialize(object obj)
+        public string Serialize(object obj, bool propertyNameToLowerCase = false)
         {
             var stringBuilder = new StringBuilder();
             stringBuilder.AppendLine("{");
             foreach (var property in obj.GetType().GetProperties())
             {
-                var name = property.Name;
+                var name = propertyNameToLowerCase 
+                    ? property.Name.ToLower() 
+                    : property.Name;
                 var propertyValue = property.GetValue(obj);
                 if (propertyValue == null) continue;
-                var value = GetPropertyValueAsString(propertyValue);
+                var value = GetPropertyValueAsString(propertyValue, propertyNameToLowerCase);
                 stringBuilder.AppendLine(name + ": " +  value);
             }
             stringBuilder.AppendLine("}");
@@ -34,12 +37,13 @@ namespace GraphQlHelper.Serializer
         /// Determines the type and returns a serialized value of the <paramref name="obj"/>.
         /// </summary>
         /// <param name="obj"></param>
+        /// <param name="propertyNameToLowerCase">A parameter to create the property names in lower case</param>
         /// <returns></returns>
-        private string GetPropertyValueAsString(object obj)
+        private string GetPropertyValueAsString(object obj, bool propertyNameToLowerCase)
         {
             if (obj is IEnumerable<object> enumerable)
             {
-                return GetEnumerableValueAsString(enumerable);
+                return GetEnumerableValueAsString(enumerable, propertyNameToLowerCase);
             }
             if (obj is string stringValue)
             {
@@ -49,19 +53,20 @@ namespace GraphQlHelper.Serializer
             {
                 return obj.ToString();
             }
-            return Serialize(obj);
+            return Serialize(obj, propertyNameToLowerCase);
         }
 
         /// <summary>
         /// Returns a serialized string of the <paramref name="enumerable"/>.
         /// </summary>
         /// <param name="enumerable"></param>
+        /// <param name="propertyNameToLowerCase">A parameter to create the property names in lower case</param>
         /// <returns></returns>
-        private string GetEnumerableValueAsString(IEnumerable<object> enumerable)
+        private string GetEnumerableValueAsString(IEnumerable<object> enumerable, bool propertyNameToLowerCase)
         {
             var stringBuilder = new StringBuilder();
             stringBuilder.Append("[ ");
-            foreach (var serializedItem in enumerable.Select(i => GetPropertyValueAsString(i)))
+            foreach (var serializedItem in enumerable.Select(i => GetPropertyValueAsString(i, propertyNameToLowerCase)))
             {
                 stringBuilder.AppendLine(serializedItem);
             }
